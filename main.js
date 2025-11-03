ef70dd4712c6704294e1d62ee3052c185f7755c0
tylerdonovan2 <tylerdonovan1506@gmail.com>
2025-11-03 17:07:26 -0500

    Small nav bugfix

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const startButton = document.querySelector("body > div > button")
+    const startButton = document.querySelector("#start-button")
     startButton.addEventListener("click", () => {
+        console.log("CLICKED!")
         window.location.href = "lift"
     })
-}
+})
